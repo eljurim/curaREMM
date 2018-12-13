c764c364782a6851377634bbcda71a47e6bb269d
memolop10 <inglopezvargas@gmail.com>
2018-12-13 14:23:34 -0600

    primer commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,24 @@
+import React , { Component } from 'react';
+
+import './Header.css'
+class Header extends Component{
+    render(){
+        return(
+        <div>
+            <div className = "title">
+            <h1>CuraREMM</h1>
+            </div>
+                <button className="mapa">mapa</button>
+                <button className="encuentra">Encuentra</button>
+                <button className="agenda">Agenda</button>
+                
+            <div className="login">
+                <button className="register">Registro</button>
+                <button className="log">Login</button>
+            </div>
+
+        </div>
+        )}
+}
+
+export default Header;
