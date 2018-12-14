9c17e02694f10882e735da4020fcc2321a5bd458
memolop10 <inglopezvargas@gmail.com>
2018-12-14 13:45:28 -0600

    el push de manuel

--- src/Components/Tarjeta/index.js
+++ src/Components/Tarjeta/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import '../Tarjeta/Tarjeta.css'
-
+import Doctor from '../Doctor'
 class Tarjeta extends Component{
     render(){
 
@@ -17,9 +17,23 @@ class Tarjeta extends Component{
             "telefono": 66840957
         }]
 
+        let doctorRender = doctors.map(doctor =>{
+                let {name,especialidad,puntuacion,telefono} = doctor;
+                return(
+                        <Doctor name={name}
+                        especialidad={especialidad}
+                        puntuacion={puntuacion}
+                        telefono={telefono}>
+
+                        </Doctor>
+                )
+            })
+        
+
+       
         return(
             <div className="tarjeta">
-            
+            {doctorRender}
                
             </div>
         )
