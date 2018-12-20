1daf2ad026aa13e448f3d318c963b04d01b4e38b
rrortegaa <rrortegaa@gmail.com>
2018-12-20 16:06:29 -0600

    cambios al header

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -8,18 +8,22 @@ class Content extends Component {
         <div className = 'principal-content'>
           <div className = 'specialties-content'>
             <h2>Encuentra tu servicio médico de confianza</h2>
-          <select className="custom-select" onChange={this.listTester} id="inputGroupSelect04" aria-label="Example select with button addon">
-                <option selected>Choose...</option>
-                <option value='Cirugia Plastica y Reconst.'>Cirugia Plastica y Reconst.</option>
-                <option value='Ortopedia y Traumatologia'>Ortopedia y Traumatologia</option>
-                <option value='Oftalmologia'>Oftalmologia</option>
-                <option value='Medicina Interna'>Medicina Interna</option>
+            <select
+              className = "custom-select"
+              onChange = {this.listTester}
+              id = "inputGroupSelect04"
+              aria-label = "Example-select-with-button-addon">
+                <option selected> ESPECIALIDAD </option>
+                <option value = 'Cirugia Plastica y Reconst.'>Cirugia Plastica y Reconst.</option>
+                <option value = 'Ortopedia y Traumatologia'>Ortopedia y Traumatologia</option>
+                <option value = 'Oftalmologia'>Oftalmologia</option>
+                <option value = 'Medicina Interna'>Medicina Interna</option>
                 <option value='Ginecologia y Obst.'>Ginecologia y Obst.</option>
                 <option value='Medicina de Rehabilitacion'>Medicina de Rehabilitacion</option>
-              </select>
-            <div className = 'specialties-dropdown'>
+            </select>
+            {/* <div className = 'specialties-dropdown'>
               <a
-                className="btn btn-default dropdown-toggle"
+                className="dropdown-toggle"
                 href="#"
                 role="button"
                 id="dropdownMenuLink"
@@ -27,7 +31,7 @@ class Content extends Component {
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-              ESPECIALIDADES  
+              ESPECIALIDADES   
               </a>
             </div>
             <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
@@ -35,7 +39,7 @@ class Content extends Component {
               <a className="dropdown-item" href="#">Oncologo</a>
               <a className="dropdown-item" href="#">Dermatologo</a>
               <a className="dropdown-item" href="#">Cardiologo</a>
-            </div>
+            </div> */}
 
             <div className = 'buttons'>
             <button className = 'near btn btn-primary'>MÁS CERCANO</button>
